f099f177b67cf904b23a87612b05b5d4635e293b
WOOSAH <31331596+Smiley-McSmiles@users.noreply.github.com>
2024-10-11 10:16:35 -0500

    test8

--- setup.sh
+++ setup.sh
@@ -169,11 +169,11 @@ GetArchitecture(){
 }
 
 InstallDependencies(){
-	packagesNeededDebian=(libva libva2 mesa-va-drivers mesa-vdpau-drivers ffmpeg git net-tools openssl bc screen curl wget tar)
-    packagesNeededRHEL=(libva libva-utils libva-vdpau-driver libva-intel-media-driver libva-intel-driver libva-nvidia-driver mesa-va-drivers mesa-vdpau-drivers ffmpeg ffmpeg-devel ffmpeg-libs libicu git openssl bc screen curl wget tar)
-    packagesNeededArch=(libva-utils libva-nvidia-driver libva-intel-driver libva-mesa-driver vulkan-radeon ffmpeg git openssl bc screen curl wget tar)
-    packagesNeededOpenSuse=(libva libva2 mesa-libva libva-utils libva-vdpau-driver mesa-libva mesa-gallium mesa-drivers ffmpeg-4 git openssl bc screen curl wget tar)
-    echo "> Preparing to install needed dependancies for Jellyfin and Jellyman..."
+	packagesNeededDebian='libva libva2 mesa-va-drivers mesa-vdpau-drivers ffmpeg git net-tools openssl bc screen curl wget tar'
+	packagesNeededRHEL='libva libva-utils libva-vdpau-driver libva-intel-media-driver libva-intel-driver libva-nvidia-driver mesa-va-drivers mesa-vdpau-drivers ffmpeg ffmpeg-devel ffmpeg-libs libicu git openssl bc screen curl wget tar'
+	packagesNeededArch='libva-utils libva-nvidia-driver libva-intel-driver libva-mesa-driver vulkan-radeon ffmpeg git openssl bc screen curl wget tar'
+	packagesNeededOpenSuse='libva libva2 mesa-libva libva-utils libva-vdpau-driver mesa-libva mesa-gallium mesa-drivers ffmpeg-4 git openssl bc screen curl wget tar'
+	echo "> Preparing to install needed dependancies for Jellyfin and Jellyman..."
 
 	if [ -f /etc/os-release ]; then
 		source /etc/os-release
@@ -191,12 +191,9 @@ InstallDependencies(){
 			if (( $VERSION_ID < 9 )); then
 				crbOrPowertools="powertools"
 			else
-				packagesRemoved=(libva-vdpau-driver libva-intel-media-driver libva-nvidia-driver mesa-va-drivers mesa-vdpau-drivers)
-				echo "> RHEL 9 detected, removing unavailable packages: $packagesRemoved"
+				echo "> RHEL 9 detected, removing unavailable packages: libva-vdpau-driver libva-intel-media-driver libva-nvidia-driver mesa-va-drivers mesa-vdpau-drivers"
 				echo "> Please compile from source VAAPI drivers to take advantage of hardware acceleration"
-				for pkg in ${packagesRemoved[@]}; do
-					packagesNeededRHEL=("${packagesNeededRHEL[@]/$pkg}")
-				done
+				packagesNeededRHEL=$(echo "$packagesNeededRHEL" | sed 's/ libva-vdpau-driver//g' | sed 's/ libva-intel-media-driver//g' | sed 's/ libva-nvidia-driver//g' | sed 's/ mesa-va-drivers//g' | sed 's/ mesa-vdpau-drivers//g')
 				crbOrPowertools="crb"
 			fi
 		fi
@@ -204,21 +201,21 @@ InstallDependencies(){
 			case "$ID" in
 				fedora)
 					dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-					dnf install "${packagesNeededRHEL[@]}" -y
+					dnf install $packagesNeededRHEL -y
 					sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld
 					sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld ;;
 				rhel)
 					dnf install epel-release -y
 					dnf config-manager --set-enabled $crbOrPowertools
-					dnf install --nogpgcheck https://mirrors.trpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm \
+					dnf install --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm \
 					https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %rhel).noarch.rpm -y
-					dnf install "${packagesNeededRHEL[@]}" -y ;;
+					dnf install $packagesNeededRHEL -y ;;
 				debian | ubuntu | linuxmint | elementary)
-					apt install "${packagesNeededDebian[@]}" -y ;;
+					apt install $packagesNeededDebian -y ;;
 				arch | endeavouros | manjaro)
-					pacman -Syu "${packagesNeededArch[@]}" ;;
+					pacman -Syu $packagesNeededArch ;;
 				opensuse*)
-					zypper install "${packagesNeededOpenSuse[@]}" ;;
+					zypper install $packagesNeededOpenSuse ;;
 			esac
 	else
 		osDetected=false
